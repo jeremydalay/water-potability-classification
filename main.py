602bc10730809337e7b8fbf344cb232422df281f
Jeremy Dalay <jeremydalay@gmail.com>
2022-07-14 16:01:34 +0800

    Up

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ st.set_page_config(
 
 data = pd.read_csv('https://github.com/jeremydalay/water-potability-classification/blob/main/dataset/water_potability_final.csv?raw=true',lineterminator='\n')
 
-cred = credentials.Certificate("https://github.com/jeremydalay/water-potability-classification/blob/main/data-logs-f460d-firebase-adminsdk-wf2z1-4ddd95bf0f.json")
+cred = credentials.Certificate("https://github.com/jeremydalay/water-potability-classification/blob/main/data-logs-f460d-firebase-adminsdk-wf2z1-4ddd95bf0f.json?raw=true")
 firebase_admin.initialize_app(cred)
 
 
